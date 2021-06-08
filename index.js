2c26e6ba872e6ebf67bb7b7ee2242e537a948f67
Nevold <nevold@tut.by>
2021-06-08 23:36:14 +0300

    method

--- index.js
+++ index.js
@@ -10,19 +10,61 @@ const getCars = async (page, limit = 7) => {
 };
 
 const getCar = async (id) => {
-  return console.log(await (await fetch(`${garage}/${id}`)).json());
+  const rensponce = await fetch(`${garage}/${id}`);
+  return console.log(await rensponce.json());
 };
 
-// getCar(2);
-// const createCar = async () => {
-//   const rensp = await fetch(`${garage}`);
-//   return console.log(JSON.stringify());
-// };
-// createCar();
-const deleteCar = async (id) => {
-  const a = await (await fetch(`${garage}/${id}`)).json();
-  console.log(a);
-  // return (await fetch(`${garage}/${id}`), { method: 'DELETE' }).json();
+const createCar = async (body) => {
+  const rensponce = await fetch(`${garage}`, {
+    method: 'POST',
+    body: JSON.stringify(body),
+    headers: { 'Content-Type': 'application/json' },
+  });
+  return await rensponce.json();
 };
-deleteCar(3);
-getCars(1, 4);
+
+const deleteCar = async (id) => {
+  const rensponce = await fetch(`${garage}/${id}`, { method: 'DELETE' });
+  return await rensponce.json();
+};
+
+const updateCar = async (id, body) => {
+  const rensponce = await fetch(`${garage}/${id}`, {
+    method: 'PUT',
+    body: JSON.stringify(body),
+    headers: { 'Content-Type': 'application/json' },
+  });
+  return await rensponce.json();
+};
+
+const startEngineCar = async (id) => {
+  const rensponce = await fetch(`${engine}?id=${id}&status=started`);
+  return console.log(await rensponce.json());
+};
+
+const stopEngineCar = async (id) => {
+  const rensponce = await fetch(`${engine}?id=${id}&status=stopped`);
+  return console.log(await rensponce.json());
+};
+
+const driveCar = async (id) => {
+  const rensponce = await fetch(`${engine}?id=${id}&status=drive`).catch();
+  return console.log(
+    rensponce.status !== 200
+      ? {
+          success: false,
+        }
+      : await rensponce.json()
+  );
+};
+// getCar(9);
+
+// deleteCar(6);
+// getCars(1);
+// updateCar(9, {
+//   name: 'BMW444',
+//   color: '#fede00',
+// });
+// startEngineCar(1);
+// stopEngineCar(5);
+// driveCar(1);
